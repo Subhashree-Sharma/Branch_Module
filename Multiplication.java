54ec8d2b1a5553fad5daf6a608ddb2afae389d22
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-01-07 16:08:28 +0530

    Initial commit - Multiplication code

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Multiplication code");
+        int a = 10;
+        int b = 20; 
+        int c = 10;
+        System.out.println(a*b*c);
+
+    }
+}
